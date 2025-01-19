4b3ba77caa73527d79f267eca1af6a7e7a61f74e
Fitswc <ericchufengzeng@163.com>
2025-01-19 12:37:46 +0800

    Reorder

--- gfx/models/portraits/umamusume/ConvertDDS.bat
+++ gfx/models/portraits/umamusume/ConvertDDS.bat
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7c73e8f2144bd2e22349773a8dbae8b4311957c7b69922b5cca39edd2a6df988
-size 109
+for %%f in (*.png) do (
+  magick "%%~nf.png" -set colorspace sRGB -define dds:compression=NONE "%%~nf.DDS"
+)
